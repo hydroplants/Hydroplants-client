017e1e520a6a40f30e8783a306cf3a706b7883b8
Nikhil Yadav <nikhilyadav1397@gmail.com>
2023-10-06 18:17:52 +0200

    Getting data from RPi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,27 @@ import React from "react";
 import '../css/home.css';
 import {io} from 'socket.io-client'
 
+const cors = require('cors');
+
 const Home = () => {
-    const [temperature, setTime] = React.useState('fetching')  
-  
-    React.useEffect(()=>{
-        const socket = io('http://localhost:9002')
-        socket.on('connect', ()=>console.log(socket.id))
-        socket.on('connect_error', ()=>{
-            setTimeout(()=>socket.connect(),9002)
-        })
-        socket.on('temperature', (data)=>setTime(data))
-        socket.on('disconnect',()=>setTime('server disconnected'))
-    },[])
+    
+    const [temperature, setTemperature] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/api/data');
+                const data = await response.json();
+                setTemperature(data.temperature);
+            } catch (error) {
+                console.error('Error fetching live data:', error);
+            }
+        };
+
+        const interval = setInterval(fetchData, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="div-1">Temperature: {temperature}</div>
