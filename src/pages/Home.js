888243e594ff3ab1e9ec3c30b3dd2c18d2f03432
Nikhil Yadav <nikhilyadav1397@gmail.com>
2023-10-07 14:05:23 +0200

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,32 +2,6 @@ import React from "react";
 import './home.css';
 
 const Home = () => {
-    
-    const [temperature, setTemperature] = React.useState([]);
-
-    React.useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://flowing-aware-kangaroo.ngrok-free.app/api/data/', {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'ngrok-skip-browser-warning': '654789'
-                    }
-                });
-                console.log(response);
-                const data = await response.clone().json();
-                console.log(data.temperature);
-                setTemperature(data.temperature);
-            } catch (error) {
-                console.error('Error fetching live data:', error);
-            }
-        };
-
-        const interval = setInterval(fetchData, 5000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <div>
             <div className="home-head">
